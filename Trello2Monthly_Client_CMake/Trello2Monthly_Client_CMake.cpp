52038ad8ecdc61febca9f33695ca5f4e24cd790b
Phuong <phuong.tran@jvncomm.com>
2019-04-29 09:50:29 -0400

    Init

---
+++ Trello2Monthly_Client_CMake/Trello2Monthly_Client_CMake.cpp
@@ -0,0 +1,15 @@
+﻿// Trello2Monthly_Client_CMake.cpp : Defines the entry point for the application.
+//
+
+#include "Trello2Monthly_Client_CMake.h"
+
+using namespace std;
+
+int main()
+{
+	monthly new_month;
+	new_month.run();
+	new_month.shutdown();
+	std::getchar();
+	return 0;
+}
---
+++ Trello2Monthly_Client_CMake/Trello2Monthly_Client_CMake.h
@@ -0,0 +1,8 @@
+﻿// Trello2Monthly_Client_CMake.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
